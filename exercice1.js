6ea9832c778b9e23689a4269afd5bccc87fe984d
JulienGoMo <56203259+JulienGoMo@users.noreply.github.com>
2019-10-09 17:19:34 -0400

    Add files via upload

--- exercice1.js
+++ exercice1.js
@@ -1,6 +1,7 @@
-const city='Palm Beach'
-const name='Julien'
-let full=`I live in ${city} and my name is ${name}!`
-console.log(full)
+const city='Palm Beach';
+const name='Julien';
+let full=`I live in ${city} and my name is ${name}!`;
+console.log(full);
 
-const wife='Penlope'
+const a=Number('5');
+console.log(a*2);
